4b1c3d2d0f63e1d72289b528c320dd2c5e1aff53
Jack Skiles <jackskiles420@gmail.com>
2020-06-18 19:35:25 -0400

    updated some api requests

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,70 @@
 var express = require('express');
 var router = express.Router();
-const db = require('../models')
+const db = require('../models');
+
 /* GET home page. */
 router.get('/recipes', function(req, res, next) {
   db.Recipes.findAll(
     {
-      include: [db.Categories]
-    }
-  ).then((data) => res.json(data))
+      include: [{
+        model: db.Categories,
+        through: {
+          attributes: []
+        }
+      }
+    ]}
+  ).then((data) => {
+    res.json(data);
+  }); 
 });
+
+router.get('/recipes/:id', (req, res) => {
+  db.Recipes.findByPk(req.params.id, {
+    include: [{
+      model: db.Categories,
+      through: {
+        attributes: []
+      }
+    }]
+  })
+  .then(data => {
+    res.json(data);
+  })
+})
+
+router.post('/recipes', (req, res) => {
+  const { name, review, description, url, likes, vegetarian, vegan, glutenfree, categories } = req.body
+
+  console.log(categories);
+  console.log(req.body);
+  if (!name) { res.status(400).json({ error: 'name field is required' }); }
+  if (!review) { res.status(400).json({ error: 'review field is required' }); }
+  if (!url) { res.status(400).json({ error: 'url field is required' }); }
+
+  db.Recipes.create({
+    name: name,
+    review: review,
+    description: description || '',
+    url: url,
+    likes: likes || 0,
+    vegetarian: vegetarian || false,
+    vegan: vegan || false,
+    glutenfree: glutenfree || false,
+  })
+    .then(recipe => {
+      return recipe.addCategories(categories)
+      .then(categories => {
+        res.status(201).json(recipe);
+      })
+    })
+      .catch(error => {
+        if (error.name === 'SequelizeForeignKeyConstrainError') {
+          res.json({error: 'Could not find all categories'})
+        } else {
+          res.json({error: 'Server Error'});
+          console.log(error);
+        }
+      })
+})
 
 module.exports = router;
