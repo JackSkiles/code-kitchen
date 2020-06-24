822e18d503c89be10618b1693ce4d5acabd1db09
Jack Skiles <jackskiles420@gmail.com>
2020-06-23 21:54:21 -0400

    updated like button

--- client/src/components/RecipeDetail.jsx
+++ client/src/components/RecipeDetail.jsx
@@ -1,54 +1,60 @@
 import React, { Component } from 'react'
+import LikesButton from './LikesButton';
 
-
-export default class RecipeDetail extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             details: {},
-             loading: true
-        }
+export default class RecipeDetails extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      details: {},
+      loading: true,
     }
-    
-    componentDidMount() {
-        const { id } = this.props.match.params;
-        fetch(`/api/v1/recipes/${id}`)
-            .then(res => res.json())
-            .then(data => {
-                this.setState({details: data, loading: false})
-            })
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    fetch(`/api/v1/recipes/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          details: data,
+          loading: false,
+        })
+      })
+  }
+
+  render() {
+      const container = {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center'
+    }
+    const card = {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '10px',
+        border: 'solid 2px black',
+        width: '30%',
+        margin: '10px',
+        fontSize: '5vh',
+        backgroundColor: 'white'
+    }
+    const { loading, details } = this.state;
+
+    if (loading) {
+      return <div>Loading...</div>
     }
 
-    render() {
-        const container = {
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'center'
-        }
-        const card = {
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: '10px',
-            border: 'solid 2px black',
-            width: '30%',
-            margin: '10px',
-            fontSize: '10vh',
-            backgroundColor: 'white'
-        }
-        const { loading, details } = this.state;
-        if (loading) {
-            return <div>Loading</div>
-        }
-
-        return (
-            <div className="RecipeDetail" style={container}>
-                <div style={card}>
-                    {details.name}
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className="RecipeDetails" style={container}>
+        <div style={card}> 
+        <h1>{ details.name }</h1>
+          <p>{details.description}</p>
+            <LikesButton id={details.id} />
+        </div>
+      </div>
+    )
+  }
 }
--- client/src/components/Recipes.jsx
+++ client/src/components/Recipes.jsx
@@ -1,92 +1,85 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-
+import LikesButton from './LikesButton';
 
 export default class Recipes extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            recipes: [],
-            removed: []
-        }
+    this.state = {
+      recipes: [],
     }
+  }
 
-    componentDidMount(){
-        fetch('/api/v1/recipes')
-            .then(res => res.json())
-            .then(data => {
-                this.setState({ recipes: data }, () => {
-                    console.log(this.state.recipes)
-                })
-            })
+  componentDidMount() {
+    fetch('/api/v1/recipes')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          recipes: data
+        })
+      })
+  }
+
+  handleLike = (id, newLikes) => {
+    const newRecipe = this.state.recipes.find((recipe)=> recipe.id === id)
+    newRecipe.likes = newLikes
+    const newRecipes = this.state.recipes.map(recipe => {
+      if (recipe.id === id) {
+        return newRecipe
+      }
+      return recipe;
+    })
+    this.setState({
+      recipes: newRecipes
+    })
+  }
+
+  render() {
+      
+    const container = {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center'
     }
-
-    remove = (indexToRemove) => {
-        console.log("Hello")
-        fetch(`/api/v1/recipes/${this.state.recipes[indexToRemove].id}`, {
-            method: 'delete'
-        })
-        const removed = this.state.recipes.filter((recipes, index) => {
-            return index != indexToRemove;
-        })
-        this.setState({recipes: removed}, () => {
-          console.log(removed);
-        })
+    const outerCard = {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '10px',
+        border: 'solid 2px black',
+        width: '30%',
+        margin: '10px',
+        backgroundColor: 'rgba(189, 232, 234)' 
     }
-
-    // remove = (indexToRemove) => {
-    //     fetch(`/api/v1/recipes/${indexToRemove}`)
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             console.log(data)
-    //         })
-    // }
-    render() {
-        const container = {
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'center'
-        }
-        const outerCard = {
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: '10px',
-            border: 'solid 2px black',
-            width: '30%',
-            margin: '10px',
-            backgroundColor: 'rgba(189, 232, 234)' 
-        }
-        const innerCard = {
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: '10px',
-            border: 'solid 2px black',
-            width: '90%',
-            margin: '10px',
-            backgroundColor: 'white'
-        }
-        return (
-            <div>
-                { this.state.recipes.map((recipe, index) => {
-                    return (
-                        <div style={container}>
-                            <div key={recipe.id}  style={outerCard}>
-                                <h2>{ recipe.name }</h2>
-                                <div key={recipe.id} style={innerCard}>
-                                    <h4>{ recipe.review }</h4>
-                                    <Link to={`/recipes/${recipe.id}`}>Show Details</Link>
-                                    <button type="submit" onClick={() => this.remove(index) }>Delete</button>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })}
+    const innerCard = {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '10px',
+        border: 'solid 2px black',
+        width: '90%',
+        margin: '10px',
+        backgroundColor: 'white'
+    }
+    return (
+      <div style={container}>
+        { this.state.recipes.map(recipe => {
+          return (
+            <div key={recipe.id} style={outerCard} >
+              <h1>{ recipe.name }</h1>
+              <div style={innerCard}>
+                <p>{ recipe.description }</p>
+                <Link to={`/recipes/${recipe.id}`}>Show Details</Link>
+                <LikesButton id={recipe.id} />
+              </div>
             </div>
-        )
-    }
+          )
+        })}
+        <Link to="/recipes/new">Submit a review</Link>
+      </div>
+    )
+  }
 }
