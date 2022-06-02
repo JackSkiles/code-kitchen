1615542ef28f2a149af58d6aa46d6533712ce450
Jack Skiles <jackskiles420@gmail.com>
2022-06-02 15:49:24 -0400

    maybe fixed heroku bug:

--- config/config.js
+++ config/config.js
@@ -7,7 +7,13 @@ module.exports =
     "password": process.env.DB_PASSWORD || null,
     "database": process.env.DB_DATABASE || "codekitchen",
     "host": process.env.DB_HOST || "127.0.0.1",
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions": {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false
+      }
+   },
   },
   
   "production": {
