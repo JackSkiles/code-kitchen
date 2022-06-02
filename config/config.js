ce2e3ca4c06cd71c55fe7c6a1c6a1c25937e73d8
Jack Skiles <jackskiles420@gmail.com>
2022-06-02 14:48:53 -0400

    changed files

---
+++ config/config.js
@@ -0,0 +1,30 @@
+// {
+//   "development": {
+//     "username": "postgres",
+//     "password": "postgres",
+//     "database": "codekitchen",
+//     "host": "127.0.0.1",
+//     "dialect": "postgres"
+//   },
+  
+//   "production": {
+//     "use_env_variable": "HEROKU_POSTGRESQL_CHARCOAL_URL"
+// }
+// }
+
+require('dotenv').config();
+
+module.exports = 
+{
+  "development": {
+    "username": process.env.DB_USERNAME || "postgres",
+    "password": process.env.DB_PASSWORD || null,
+    "database": process.env.DB_DATABASE || "codekitchen",
+    "host": process.env.DB_HOST || "127.0.0.1",
+    "dialect": "postgres"
+  },
+  
+  "production": {
+    "use_env_variable": "DATABASE_URL"
+  }
+}
