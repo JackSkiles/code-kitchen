c30a39b62721de5cb33bf17cd5801c3426496fb3
Jack Skiles <jackskiles420@gmail.com>
2022-06-05 12:23:00 -0400

    fixed sequelize issues on localhost

--- config/config.js
+++ config/config.js
@@ -4,19 +4,19 @@ module.exports =
 {
   "development": {
     "username": process.env.DB_USERNAME || "postgres",
-    "password": process.env.DB_PASSWORD || null,
+    "password": process.env.DB_PASSWORD || "null",
     "database": process.env.DB_DATABASE || "codekitchen",
     "host": process.env.DB_HOST || "127.0.0.1",
+    "dialect": "postgres"
   },
   
   "production": {
     "use_env_variable": "DATABASE_URL",
     "dialect": "postgres",
-    "dialectOptions": {
-      ssl: {
-          require: true,
-          rejectUnauthorized: false
-      }
-   },
+    // "dialectOptions": {
+    //   ssl: {
+    //       require: true,
+    //       rejectUnauthorized: false
+    //   }
 }
 }
